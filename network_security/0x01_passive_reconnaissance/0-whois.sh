c2b2afa746caf5482fce7cd87ec477997ad2e701
camia97 <camilaabdala26@gmail.com>
2024-08-06 21:25:26 -0400

    add change file

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -FS': ' '/^Registrant/ || /^Admin/ || /^Tech/ {print $1, $2}' > holberton.csv
+whois $1 | awk -F: '/^Registrant/ || /^Admin/ || /^Tech/ {print $1 "," $2}' > holberton.csv
