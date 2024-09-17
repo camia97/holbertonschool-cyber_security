96f66a633a3d9bc941ac011c15a11e10d421185f
camia97 <camilaabdala26@gmail.com>
2024-09-16 20:19:42 -0400

    add xor decoded

---
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+test_pass="$1"
+test_pass="${test_pass#'{xor}'}"
+decode_test_pass=$(echo "$test_pass" | base64 -d)
+
+decode_test_pass_xor=""
+for((i=0; i < ${#decode_test_pass}; i++)); do
+	char="${decode_test_pass:$i:1}"
+	ascii_form=$(printf "%d" "'$char")
+	xor_res=$((ascii_form ^ 95))
+	decode_test_xor+="$(printf "$(printf '\\x%x' $xor_res)")"
+done
+
+echo -e "$decode_test_xor"
