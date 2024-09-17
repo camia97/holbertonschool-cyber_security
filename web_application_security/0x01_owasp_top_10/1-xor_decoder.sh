d5f9d91bcc0eb599e2f36451352dbcb73d528538
camia97 <camilaabdala26@gmail.com>
2024-09-16 20:49:18 -0400

    change in code

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 pass="$1"
 pass="${pass#'{xor}'}"
-decoded=$(echo "$pass" | base64 -d)
+decoded_pass=$(echo "$pass" | base64 -d)
 
 decoded_pass_xor=""
-for ((i = 0; i < ${#decoded}; i++)); do
-    char="${decoded:$i:1}"
+for ((i = 0; i < ${#decoded_pass}; i++)); do
+    char="${decoded_pass:$i:1}"
     ascii_value=$(printf "%d" "'$char")
     xor_result=$(( ascii_value ^ 95 ))
     decoded_pass_xor+="$(printf "$(printf '\\x%x' $xor_result)")"
