59dbd40b29893143228ae6a6b5ca71a748aea634
camia97 <camilaabdala26@gmail.com>
2024-09-25 14:10:22 -0400

    change file

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -10,8 +10,8 @@ fi
 pass="$1"
 pass="${pass#'{xor}'}"
 
-# Decodifica la cadena base64, asegurando que no se generen null bytes
-decoded_pass=$(echo "$pass" | base64 -d 2>/dev/null)
+# Decodifica la cadena base64
+decoded_pass=$(echo -n "$pass" | base64 -d 2>/dev/null)
 
 # Verifica si la decodificación fue exitosa
 if [ $? -ne 0 ]; then
@@ -25,9 +25,9 @@ for ((i = 0; i < ${#decoded_pass}; i++)); do
     char="${decoded_pass:$i:1}"
     ascii_value=$(printf "%d" "'$char")
     xor_result=$(( ascii_value ^ 95 ))
-    decoded_pass_xor+="$(printf "$(printf '\\x%x' $xor_result)")"
+    decoded_pass_xor+=$(printf "\\$(printf '%03o' "$xor_result")")
 done
 
-# Muestra el resultado
-echo -e "$decoded_pass_xor"
+# Muestra el resultado sin nuevos espacios o líneas
+echo -n "$decoded_pass_xor"
 
