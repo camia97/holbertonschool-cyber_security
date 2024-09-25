c5c9cccc795e3b8d4b021b9664ba7d6cda1bdbff
camia97 <camilaabdala26@gmail.com>
2024-09-25 14:06:13 -0400

    change file

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+# Asegúrate de que se pasa un argumento
+if [ "$#" -ne 1 ]; then
+  echo "Uso: $0 {xor}HASH"
+  exit 1
+fi
+
+# Extraer el hash del argumento
 pass="$1"
 pass="${pass#'{xor}'}"
+
+# Decodificar la cadena base64
 decoded_pass=$(echo "$pass" | base64 --decode)
 
 decoded_pass_xor=""
+# Realizar la operación XOR
 for ((i = 0; i < ${#decoded_pass}; i++)); do
     char="${decoded_pass:$i:1}"
     ascii_value=$(printf "%d" "'$char")
@@ -11,4 +22,6 @@ for ((i = 0; i < ${#decoded_pass}; i++)); do
     decoded_pass_xor+="$(printf "$(printf '\\x%x' $xor_result)")"
 done
 
+# Mostrar el resultado
 echo -e "$decoded_pass_xor"
+
