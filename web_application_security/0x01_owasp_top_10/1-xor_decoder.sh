6e8a142e07e97e10c6957d1df81b871e67caf918
camia97 <camilaabdala26@gmail.com>
2024-09-26 10:29:46 -0400

    new file

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
-pass="$1"
-pass="${pass#'{xor}'}"
-decoded_pass=$(echo "$pass" | base64 -d)
 
+# Verify that an argument is pass
+if [ -z "$1" ]; then
+    echo "Uso: ./1-xor_decoder.sh {xor}encoded_string"
+    exit 1
+fi
+
+# Remove '{xor}'
+encoded_pass="${1#'{xor}'}"
+
+# Base64 decode y remove null bytes
+decoded_pass=$(echo "$encoded_pass" | base64 -d 2>/dev/null | tr -d '\0')
+
+# XOR with the WebSphere key (0x5f o 95)
 decoded_pass_xor=""
-for ((i = 0; i < ${#decoded_pass}; i++)); do
+for (( i=0; i<${#decoded_pass}; i++ )); do
     char="${decoded_pass:$i:1}"
     ascii_value=$(printf "%d" "'$char")
     xor_result=$(( ascii_value ^ 95 ))
-    decoded_pass_xor+="$(printf "$(printf '\\x%x' $xor_result)")"
+
+    decoded_pass_xor+="$(printf '\\x%x' "$xor_result")"
 done
 
+# Show the password decoded
 echo -e "$decoded_pass_xor"
+
