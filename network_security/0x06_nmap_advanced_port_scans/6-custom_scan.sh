db08d728884688b44a8ed7e21b342cd2fa375f3e
camia97 <camilaabdala26@gmail.com>
2024-11-05 10:30:09 -0500

    changes to the file

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -sA --scanflags URGACKPSHRSTSYNFIN -p$2 $1 -oX d >/dev/null -oN custom_scan.txt
+sudo nmap -sA --scanflags URGACKPSHRSTSYNFIN -p$2 $1 2>&1>/dev/null -oN custom_scan.txt
