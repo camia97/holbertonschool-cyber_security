afa827bcbcd23a1c5461bbeeec284bc8570d8a3c
camia97 <camilaabdala26@gmail.com>
2024-11-19 14:48:12 -0500

    add tr to delete "

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$(awk '{print $12}' "logs.txt" | sort | uniq -c | sort -nr | head -1 | awk '{print $2}')"
+echo "$(awk '{print $12}' "logs.txt" | sort | uniq -c | sort -nr | head -1 | awk '{print $2}' | tr -d '"')"
