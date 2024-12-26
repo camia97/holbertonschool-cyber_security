630fa946524930a2d1fe14eca03b77e2b3638576
camia97 <camilaabdala26@gmail.com>
2024-12-26 11:25:34 -0500

    new file counter ids

---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require "json"
+
+def count_user_ids(path)
+    # Read the json file
+  file_content = File.read(path)
+  data = JSON.parse(file_content)
+
+  # Count the user ids
+  user_id_counts = Hash.new(0)
+  data.each do |user|
+    user_id_counts[user["userId"]] += 1
+  end
+
+  # Print the counts
+  user_id_counts.each do |user_id, count|
+    puts "#{user_id}: #{count}"
+  end
+end
