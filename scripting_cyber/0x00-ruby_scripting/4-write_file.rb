eec363da3c3bf8fab7fd29a5290da3c81341cd25
camia97 <camilaabdala26@gmail.com>
2024-12-27 07:43:48 -0500

    add puts with message

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -12,4 +12,5 @@ def merge_json_files(file1_path, file2_path)
 
     # Write the merged file
     File.write(file2_path, JSON.pretty_generate(merged_file))
+    puts 'Merged JSON written to file.json'
 end
