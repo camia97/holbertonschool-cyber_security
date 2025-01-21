97251d059957594229e9f3d8834a53e77f2e3a64
camia97 <camilaabdala26@gmail.com>
2025-01-21 08:16:14 -0500

    change format json

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -3,19 +3,19 @@
 require 'net/http'
 require 'json'
 
-def post_request(url, body_params={})
+def post_request(url, body_params = {})
     uri = URI(url)
 
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = uri.scheme == 'https'
 
     request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
-    request.body = body_params.to_json
+    request.body = body_params.to_json unless body_params.empty?
 
     response = http.request(request)
 
     status = "#{response.code} #{response.message}"
-    body = JSON.pretty_generate(JSON.parse(response.body))
+    body = response.body.strip.empty? ? "{}" : JSON.pretty_generate(JSON.parse(response.body))
 
     puts "Response status: #{status}"
     puts "Response body:"
